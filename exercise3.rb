ff449e434f17062e995a2a0ce1302840d82dbaac
longbb95 <longbuiba95@gmail.com>
2016-01-28 15:39:14 +0000

    Do Exercise3

---
+++ exercise3.rb
@@ -0,0 +1,8 @@
+person1 = { first: "A", last: "Nguyen Van" }
+person2 = { first: "B", last: "Pham Thi" }
+person3 = { first: "C", last: "Nguyen Van"}
+
+params = {}
+params[:father]=person1
+params[:mother]=person2
+params[:child]=person3
