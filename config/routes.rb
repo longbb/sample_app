078fdcb6b54576940cda96e53c9d789ac306eb76
longbb95 <longbuiba95@gmail.com>
2016-02-01 07:53:27 +0000

    Finish log in/log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   resources :users
   
   get 'users/new'
@@ -9,6 +11,10 @@ Rails.application.routes.draw do
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
 
   
   # The priority is based upon order of creation: first created -> highest priority.
